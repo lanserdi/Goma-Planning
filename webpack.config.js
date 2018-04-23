df8bfa8a40cc7d570e04ccbb68fe0c508ae0bcf2
lanserdi <lanserdi@163.com>
2018-04-23 15:47:46 +0800

    更新版本

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports = {
