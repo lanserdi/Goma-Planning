02e4fda0a72027a988d947edd4b87c179d260d7b
lanserdi <lanserdi@163.com>
2018-12-31 13:33:43 +0800

    更新server.js

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const path = require('path');
-const api = require('./api');
+const express = require('express')
+const app = express()
+const server = require('http').Server(app).listen(8000)
+const path = require('path')
 
-app.use('/static/', express.static(__dirname+'/dist/'));
-app.use('/resource/', express.static(__dirname+'/resource/'));
-app.use(express.static(__dirname+'/demo/'));
-app.use('/api/', api);
-app.listen(8000);
+app.use(express.static(path.resolve(__dirname, 'site')))
