125ef7568de06c1f4f3c2140d04fef0362361f2f
lanserdi <lanserdi@163.com>
2019-08-05 17:19:45 +0800

    不再尝试使用gulp-babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,7 @@
 const { src, dest } = require('gulp');
-const babel = require('gulp-babel');
 
-const compileJs = () => {
-  return src('src/js/*.js')
-          .pipe(babel({
-            presets: ['@babel/env'],
-            plugins: ['@babel/transform-runtime']
-          }))
-          .pipe(dest('site/public/js'))
+const defaultTask = (cb) => {
+  cb();
 }
 
-module.exports.default = compileJs
-module.exports.build = () => {
-  return 'as'
-}
+exports.default = defaultTask
